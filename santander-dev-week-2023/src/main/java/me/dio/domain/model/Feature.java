6ccdc837391851d79136cac23e9cd70cad5b33d5
Cledson do Nascimento Pereira <142537029+DevCled85@users.noreply.github.com>
2023-10-21 18:02:18 -0400

    updates

---
+++ santander-dev-week-2023/src/main/java/me/dio/domain/model/Feature.java
@@ -0,0 +1,10 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity(name = "Tb_feature")
+public class Feature extends BaseItem {
+
+}
